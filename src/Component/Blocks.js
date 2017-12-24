ae783cff56d8bea93b5d7228d0fe212b63f2c929
Christopher Fox <cfox1290@gmail.com>
2017-12-24 16:47:11 -0700

    Add css and component outlines for 10 block sections

--- src/Component/Blocks.js
+++ src/Component/Blocks.js
@@ -285,19 +285,357 @@ export class SecondarySubHeading5 extends Component {
 /* End of Secondary SubHeading */
 /* End of Heading Blocks */
 /* Start of NavBar Blocks */
+/* Start of NavBar Blocks */
+export class Navbar1 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="navbar1">
+                <ul className="navbar1-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class Navbar2 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="navbar2">
+                <ul className="navbar2-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class Navbar3 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="navbar3">
+                <ul className="navbar3-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class Navbar4 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="navbar4">
+                <ul className="navbar4-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class Navbar5 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="navbar5">
+                <ul className="navbar5-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+/* End of NavBar Blocks */
+/* Start of Sub NavBar Blocks */
+export class SubNavbar1 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="subnavbar1">
+                <ul className="subnavbar1-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class SubNavbar2 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="subnavbar2">
+                <ul className="subnavbar2-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class SubNavbar3 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="subnavbar3">
+                <ul className="subnavbar3-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class SubNavbar4 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="subnavbar4">
+                <ul className="subnavbar4-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
+export class SubNavbar5 extends Component {
+    render() {
+        const childs = React.Children.toArray(this.props.children)
+        const navBar = childs.map((x, i)=> {
+            return <li key={i}>{x}</li>
+        })
+        return (
+            <header className="subnavbar5">
+                <ul className="subnavbar5-navbar">
+                    {navBar}
+                </ul>
+            </header>
+        )
+    }
+}
 
-
+/* End of Sub NavBar Blocks */
 /* End of NavBar Blocks */
 /* Start of Paragraph Blocks */
-
-
+/* Start of Basic Paragraph Blocks */
+export class Paragraph1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="paragraph1">{childs[0]}</p>
+        )
+    }
+}
+export class Paragraph2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="paragraph2">{childs[0]}</p>
+        )
+    }
+}
+export class Paragraph3 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="paragraph3">{childs[0]}</p>
+        )
+    }
+}
+export class Paragraph4 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="paragraph4">{childs[0]}</p>
+        )
+    }
+}
+export class Paragraph5 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="paragraph5">{childs[0]}</p>
+        )
+    }
+}
+/* End of Basic Paragraphs */
+/* Start of Pull Quotes */
+export class pullQuote1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="pullQuote1">{childs[0]}</p>
+        )
+    }
+}
+export class pullQuote2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="pullQuote2">{childs[0]}</p>
+        )
+    }
+}
+export class pullQuote3 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="pullQuote3">{childs[0]}</p>
+        )
+    }
+}
+export class pullQuote4 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="pullQuote4">{childs[0]}</p>
+        )
+    }
+}
+export class pullQuote5 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <p className="pullQuote5">{childs[0]}</p>
+        )
+    }
+}
+/* End of Pull Quotes */
 /* End of Paragraph Blocks */
 /* Start of Image Blocks */
-
-
+/* Start of Circle Images */
+export class CircleImage1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="circleImage1" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+export class CircleImage2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="circleImage2" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+/* End of Circle Images */
+/* Start of Rounded Images */
+export class RoundedImage1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="roundedImage1" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+export class RoundedImage2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="roundedImage2" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+/* End of Rounded Images */
+/* Start of Square Images */
+export class SquareImage1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="squareImage1" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+export class SquareImage2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <img className="squareImage2" src={childs[0]} alt={childs[1]} />
+        )
+    }
+}
+/* End of Square Images */
 /* End of Image Blocks */
 /* Start of Button Blocks */
-
-
-
+/* Start of Flat Buttons */
+export class FlatButton1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="flatButton1">{childs[0]}</button>
+        )
+    }
+}
+export class FlatButton2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="flatButton2">{childs[0]}</button>
+        )
+    }
+}
+/* End of Flat Buttons */
+/* Start of Raised Buttons */
+export class RaisedButton1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="raisedButton1">{childs[0]}</button>
+        )
+    }
+}
+export class RaisedButton2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="raisedButton2">{childs[0]}</button>
+        )
+    }
+}
+/* End of Raised Buttons */
+/* Start of Floating Buttons */
+export class floatingButton1 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="floatingButton1">{childs[0]}</button>
+        )
+    }
+}
+export class floatingButton2 extends Component {
+    render(){
+       const childs = React.Children.toArray(this.props.children)
+        return(
+            <button className="floatingButton2">{childs[0]}</button>
+        )
+    }
+}
+/* End of Floating Buttons */
 /* End of Button Blocks */
